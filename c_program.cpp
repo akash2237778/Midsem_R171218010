b97ec3ade78ac63bdc4925d56a43e10ff75b67b1
akash2237778 <akash2237778@gmail.com>
2019-03-18 10:21:22 +0530

    added class for addition of two numbers

--- c_program.cpp
+++ c_program.cpp
@@ -1,3 +1,20 @@
 #include<iostream.h>
 using namespace std;
 
+
+class Add{
+	private:
+		int a;
+		int b;
+	public:
+		void getData(int x , int y){
+		a=x;
+		y=b;
+		}
+		int addNumbers(){
+		int r;
+		r=a+b;
+		return r;
+		}
+
+}
